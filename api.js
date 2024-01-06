acff5ddc1e5a3f911772419e64c98e6a5b25de7f
Danilo_ <108522359+DSD-Development@users.noreply.github.com>
2024-01-06 15:00:18 +0100

    Update api.js

--- api.js
+++ api.js
@@ -17,18 +17,18 @@ export class ApiD {
             if (event.ctrlKey && event.shiftKey && event.key === "I") {event.preventDefault();}
             if (event.key === "F12") {event.preventDefault();}
         });
-        console.log('%c' + siteName + ' | Protezione Applicata', 'font-family: Poppins; font-size: 15px;');
+        console.log('%c' + siteName + ' | Protezione Applicata', 'font-family: Poppins; font-size: 12px;');
     }
     createAndSetStyle(siteName, siteId) {
         var Style = WebComponent[siteId].Style;
         var styleElement = document.createElement('style');
         styleElement.textContent = Style;
         document.head.appendChild(styleElement);
-        console.log('%c' + siteName + ' | Stile Applicato', 'font-family: Poppins; font-size: 15px;');
+        console.log('%c' + siteName + ' | Stile Applicato', 'font-family: Poppins; font-size: 12px;');
     }
     createAndSetHtmlDiv(siteName, siteId) {
         var HtmlText = WebComponent[siteId].Html;
         document.body.innerHTML = HtmlText;
-        console.log('%c' + siteName + ' | Struttura Applicata', 'font-family: Poppins; font-size: 15px;');
+        console.log('%c' + siteName + ' | Struttura Applicata', 'font-family: Poppins; font-size: 12px;');
     }
 }
