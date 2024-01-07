7f41d3b633dcf7c0740cd5ff5c8264cec6c1e37e
Danilo_ <108522359+DSD-Development@users.noreply.github.com>
2024-01-07 17:07:08 +0100

    Update ApiDOkey.js

--- ApiDOkey.js
+++ ApiDOkey.js
@@ -3,12 +3,12 @@ export const ApiDOkey = [
         key: "ApiDKey-rufp-ebdk-3073",
         siteName: "Sito Web Danilo",
         siteId: "00001",
-        blockWb: true
+        blockWb: false
     },
     {
         key: "ApiDKey-ghdl-owbc-1659",
         siteName: "Compito Religione",
         siteId: "00002",
-        blockWb: false
+        blockWb: true
     }
 ];
