f068abdac339dd0db80b4ad6cf660a59f3835e41
Danilo_ <108522359+DSD-Development@users.noreply.github.com>
2024-01-08 08:30:12 +0100

    Update protection.js

--- protection.js
+++ protection.js
@@ -1,1 +1,5 @@
-
+export function getNationIsAuthorized(nationNotAuthorized) {
+    nationNotAuthorized.forEach(nation => {
+        return nationIp.toString().toLowerCase() === nation.toString().toLowerCase() ? false : true; 
+    }
+}
