538a191d533bca68409626b9d567b6f889f4caf8
Danilo_ <108522359+DSD-Development@users.noreply.github.com>
2024-01-08 20:01:32 +0100

    Delete protection.js

--- protection.js
+++
@@ -1,5 +0,0 @@
-export function getNationIsAuthorized(nationNotAuthorized) {
-    nationNotAuthorized.forEach(nation => {
-        return nationIp.toString().toLowerCase() === nation.toString().toLowerCase() ? false : true; 
-    }
-}
