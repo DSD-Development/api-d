8b7197c574f567c6b2bbce1882e71e98b7415f5e
Danilo_ <108522359+DSD-Development@users.noreply.github.com>
2024-01-09 17:55:00 +0100

    Update checkUser.js

--- functions/checkUser.js
+++ functions/checkUser.js
@@ -19,6 +19,7 @@ export function WBcheckUser(siteName, siteId) {
                 top: 50%; 
                 left: 50%;
                 transform: translate(-50%, -50%);
+                border: 2px solid white;
             }
 
             .imgApiD {
