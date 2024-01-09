e916f1c5b0b31784fec5380b0ab7c2fb67fa001e
Danilo_ <108522359+DSD-Development@users.noreply.github.com>
2024-01-09 17:22:53 +0100

    Update antiAfk.js

--- protection/antiAfk.js
+++ protection/antiAfk.js
@@ -1,14 +1,15 @@
-var lastActivityTime;
-var inactivityLimit = 10000; 
+var timer;
+var inactivityLimit = 11000; 
 
-function resetTimer() {lastActivityTime = Date.now();}
+function resetTimer() {
+      clearTimeout(timer);
+      timer = setTimeout(function() {
+            location.reload();
+      }, inactivityLimit);
+}
+
 export function antiAfk() {
     document.addEventListener('mousemove', resetTimer);
     document.addEventListener('keydown', resetTimer);
     document.addEventListener('scroll', resetTimer);
-    var currentTime = Date.now();
-    if (currentTime - lastActivityTime > inactivityLimit) {
-        return true;
-    }
-    return false;
 }
