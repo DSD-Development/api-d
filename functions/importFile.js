44eede8b4c49a00a98072d323d063d55d57db8ef
Danilo_ <108522359+DSD-Development@users.noreply.github.com>
2024-01-09 12:43:40 +0100

    Update importFile.js

--- functions/importFile.js
+++ functions/importFile.js
@@ -5,3 +5,12 @@ export function importScript(type, crossOR, src) {
     if (crossOR != "") {script.crossOrigin = crossOR;}
     document.body.appendChild(script);
 }
+
+export function createAndImportLink(crossOR, int, rel, href) {
+    const importLink = document.createElement('link');
+    importLink.href = href;
+    importLink.rel = rel;
+    importLink.integrity = int;
+    if (crossOR != "") {importLink.crossOrigin = crossOR;};
+    document.head.appendChild(importLink);
+}
